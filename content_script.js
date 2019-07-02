ca06fb2a92e951980008ca38849b93c46fbeb4ee
boyan <bx376@nyu.edu>
2019-07-03 00:25:34 +0800

    Polish naming in ./content_script.js

--- content_script.js
+++ content_script.js
@@ -1,83 +1,89 @@
-class vocabularyInfo{
-  constructor(selected_vocabulary, selected_paragraph){
-    this.vocabulary = selected_vocabulary
-    this.sentence   = getSelectionSentence(selected_paragraph, selected_vocabulary)
-    this.paragraph  = selected_paragraph
-  }
-}
-
-function getValidParagraph(anchorNode){
-  curNode = anchorNode
-  validName  = ['P','LI','H1','H2','H3','H4','H5','H6']
-  while(curNode.nodeName != "BODY"){
-    if(validName.includes(curNode.nodeName)){
-      return curNode.textContent
+class vocabularyInfo {
+    constructor(selected_vocabulary, selected_sentence, selected_paragraph) {
+        this.vocabulary = selected_vocabulary;
+        this.sentence = selected_sentence;
+        this.paragraph = selected_paragraph;
     }
-    else{
-      curNode = curNode.parentNode
+}
+
+function getSelectedVocabulary(selectedNode) {
+    return selectedNode.toString()
+}
+
+function getSelectedSentence(paragraph, vocabulary) {
+    sentences = paragraph.match(/[^\.!\?]+[\.!\?]+/g);
+    for (var index in sentences) {
+        sentence = sentences[index];
+        if (sentence.includes(vocabulary)) {
+            return sentence
+        }
     }
-  }
-  return ""
 }
 
-function getSelectionSentence(paragraph, vocabulary){
-  sentences = paragraph.match( /[^\.!\?]+[\.!\?]+/g )
-  for(var index in sentences){
-    sentence = sentences[index]
-    if(sentence.includes(vocabulary)){
-      return sentence
+function getSelectedParagraph(anchorNode) {
+    curNode = anchorNode;
+    validName = ['P', 'LI', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+    while (curNode.nodeName !== "BODY") {
+        if (validName.includes(curNode.nodeName)) {
+            return curNode.textContent
+        } else {
+            curNode = curNode.parentNode
+        }
     }
-  }
+    return ""
 }
 
+function getSelectedInfo() {
+    selectedNode = window.getSelection();
 
-function getSelectionInfo(){
-  selection = window.getSelection()
-  // exception handling 1: nothing selected
-  if(selection.anchorNode == null){ return null }
-  // end
-  selected_vocabulary = selection.toString()
-  selected_paragraph  = getValidParagraph(selection.anchorNode)
-  return new vocabularyInfo(selected_vocabulary, selected_paragraph)
+    if (selectedNode.anchorNode == null) {  // handle exception 1: nothing selected
+        return null
+    }
+
+    let selected_vocabulary = getSelectedVocabulary(selectedNode);
+    let selected_paragraph = getSelectedParagraph(selectedNode.anchorNode);
+    let selected_sentence = getSelectedSentence(selected_paragraph, selected_vocabulary);
+    return new vocabularyInfo(selected_vocabulary, selected_sentence, selected_paragraph)
 }
 
-function saveSelection(){
-  vocInfo = getSelectionInfo()
-  // exception handling 1: no vocabulary selected
-  if(vocInfo == null){ return ;}
-  // end
-  else{
-    VocabularyList = JSON.parse(localStorage.vocabularyList)
-    VocabularyList.push(vocInfo)
-    localStorage.vocabularyList = JSON.stringify(VocabularyList)
-  }
+function saveSelection() {
+    let vocabularyInfo = getSelectedInfo();
+
+    if (vocabularyInfo == null) {   // handle exception 1: nothing selected
+        return null;
+    } else {
+        var VocabularyList = JSON.parse(localStorage.vocabularyList);
+        VocabularyList.push(vocabularyInfo);
+        localStorage.vocabularyList = JSON.stringify(VocabularyList)
+    }
 }
 
-function onSelection(signal, sender, sendResponse){
-  switch(signal.action){
-    case 'save':
-      saveSelection();
-      return true;
-  }
+function onSelection(signal, sender, sendResponse) {
+    switch (signal.action) {
+        case 'save':
+            saveSelection();
+            return true;
+    }
 }
+
 function initContentScript() {
-  chrome.runtime.onMessage.addListener(onSelection);
-  // initialize the vocabularyList
-  localStorage.vocabularyList = JSON.stringify([]);
-  // chrome.runtime.sendMessage({'init': true}, onSelection);
-  // let randint = Math.random();
-  // chrome.storage.sync.set({"random number": randint}, function() {
-  //   console.log('Random number is ' + randint);
-  // });
-  // chrome.storage.sync.get(["random number"], function(randint){
-  //   console.log("fetch from local storage: " + randint);
-  // });
-  // document.addEventListener('keydown', function(evt) {
-  //   if (!document.hasFocus()) {
-  //     return true;
-  //   }
-  //   return true;
-  // }, false);
+    chrome.runtime.onMessage.addListener(onSelection);
+    // initialize the vocabularyList
+    localStorage.vocabularyList = JSON.stringify([]);
+    // chrome.runtime.sendMessage({'init': true}, onSelection);
+    // let randint = Math.random();
+    // chrome.storage.sync.set({"random number": randint}, function() {
+    //   console.log('Random number is ' + randint);
+    // });
+    // chrome.storage.sync.get(["random number"], function(randint){
+    //   console.log("fetch from local storage: " + randint);
+    // });
+    // document.addEventListener('keydown', function(evt) {
+    //   if (!document.hasFocus()) {
+    //     return true;
+    //   }
+    //   return true;
+    // }, false);
 }
 
 initContentScript();
